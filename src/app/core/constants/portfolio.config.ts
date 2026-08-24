ce377b5a898bb84d221c0f610f713dd0a8daf7b2
Santosh gupta <164666362+Santoshgupta498@users.noreply.github.com>
2026-08-24 20:00:18 +0530

    fix: use relative path for resume download

--- src/app/core/constants/portfolio.config.ts
+++ src/app/core/constants/portfolio.config.ts
@@ -9,7 +9,7 @@ export const PORTFOLIO_CONFIG: PortfolioConfig = {
   github: 'https://github.com/Santoshgupta498',
   linkedin: 'https://linkedin.com/in/santoshgupta7698',
   Instagram: 'https://www.instagram.com/santosh_gupta__07?utm_source=qr&igsi=anc5aTU2emthbnU4',
-  resume: '/assets/SantoshGupta_Dotnet_Developer_Resume.pdf',
+  resume: 'assets/SantoshGupta_Dotnet_Developer_Resume.pdf',
   bio: `I'm a passionate .NET and Angular developer specializing in building scalable applications, 
     high-performance APIs, cloud-native systems, AI-powered solutions, and futuristic digital experiences. 
     With expertise spanning distributed architectures, microservices, and intelligent automation, 
